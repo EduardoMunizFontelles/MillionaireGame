79987e4c4b5db3086eb75af9845808b9d3d25973
Eduardo Muniz Fontelles <eduardofontelles@Eduardos-MacBook-Pro.local>
2022-06-15 15:32:36 -0300

    Added a rules file

---
+++ rules.py
@@ -0,0 +1,44 @@
+def printRules():
+    print("""
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    --Welcome to Who Wants To Be A Millionaire Game!--
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    Once you enter the main game, you will be asked 
+    increasingly difficult general knowledge questions. 
+    
+    Each features four possible answers, to which 
+    you must give the correct answer. 
+    
+    Doing so wins them a certain amount of money, with tackling 
+    more difficult questions increasing their prize fund. 
+    
+    During their game, the player has a set of lifelines that they may use only 
+    once to help them with a question-(4 tips). 
+    
+    Also there are some checkpoint that garantee you won money even 
+    if you get the worng awnser.
+    
+    If a contestant feels unsure about an answer and 
+    does not wish to play on, they can walk away with the money they have won.
+    
+    Question number  |	Question value  |  Checkpoins
+            1	            £100
+            2	            £200
+            3	            £300
+            4	            £500
+            5	            £1,000              X
+            6	            £2,000
+            7	            £4,000
+            8	            £8,000
+            9	            £16,000
+            10	            £32,000             X
+            11	            £64,000
+            12	            £125,000
+            13	            £250,000            X
+            14	            £500,000
+            15	            £1,000,000
+
+""")
