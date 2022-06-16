292cc175f03105954e9cf7bd2304d2c5c9db7a42
Eduardo Muniz Fontelles <eduardofontelles@Eduardos-MacBook-Pro.local>
2022-06-16 11:11:52 -0300

    Added logic to go out with the money won

--- terminalGame.py
+++ terminalGame.py
@@ -12,11 +12,12 @@ printRules()
 acertou = True
 ganhou = False
 contador = 1
-prize = 100
+prizes = [100,200,300,500,1000,2000,4000,16000,32000,64000,125000,250000,500000,1000000]
+amountWon = 0
 
-while (acertou) and not(ganhou):
+while (acertou) and (contador <= 15):
     print(f'Rodada {contador}!')
-    print(f'Question prize: ${prize}!')
+    print(f'Question prize: ${prizes[contador-1]}!\n')
 
     if (contador <= 5):
         level = 1
@@ -26,17 +27,25 @@ while (acertou) and not(ganhou):
         level = 3
 
     gabarito = buscarPergunta(level)
+    decisao = int(input(f'Would you like to: 1-Answer, 2-Get a tip or 3-Leave with your money: ${amountWon}?'))
+    if (decisao == 1):
+        resposta = input('Very well, what is your guess then?').upper()
 
-    resposta = input('Digite a sua resposta: ').upper()
-
-    if (resposta == gabarito):
-        print('Certa Resposta!')
-        if (contador == 15):
-            print('Parabens you won!')
-            ganhou = True
-        contador += 1
-        prize *= 2
+        if (resposta == gabarito):
+            print('Certa Resposta!\n\n')
+            amountWon = prizes[contador-1]
+            contador += 1
+        else:
+            print('Resposta incorreta, burro!\n\n')
+            acertou = False  
+    elif (decisao == 2):
+        pass
     else:
-        print('Resposta incorreta, burro!')
-        acertou = False    
+        print(f'Thanks for playing. You are leaving with: ${amountWon}!\n')          
+        acertou = False
 
+if (contador == 11):
+    print('Congrats! You are a millionaire.')
+    print(f'Amount: $1.000.000')
+
+print('End of the game!')
