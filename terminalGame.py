131c1ebfbceaa0b726a078c06071d47ad55b3c6c
Eduardo Muniz Fontelles <eduardofontelles@Eduardos-MacBook-Pro.local>
2022-06-16 09:47:52 -0300

    Created a txt file to keep the questions and separate them into 3 levels of difficulty

--- terminalGame.py
+++ terminalGame.py
@@ -1,3 +1,4 @@
+from random import randint
 from rules import printRules
 
 class Player:
@@ -7,3 +8,23 @@ class GameLogic:
     pass
 
 printRules()
+
+def buscarPergunta(level):
+    with open("questions.txt", "r") as arquivo:
+        linhas = arquivo.readlines()
+        if level == 1:
+            indexSorteado = randint(0,3)
+        elif level == 2:
+            indexSorteado = randint(4,7)
+        else:
+            indexSorteado = randint(8,len(linhas))        
+        pergunta = linhas[indexSorteado].split(',')
+        enunciado = pergunta[0]
+        alternativas = pergunta[1]
+        gabarito = pergunta[2]
+        print(enunciado)
+        print(alternativas)
+        return gabarito
+
+level = int(input('Digite o nivel: '))            
+buscarPergunta(level)
